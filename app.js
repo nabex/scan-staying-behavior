4599764cd5b5cad078fab16469b739cb833e2950
Koki Watanabe <gateway0616@gmail.com>
2017-09-19 16:57:10 +0900

    [add] 現在時刻取得関数

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
 var noble = require('noble');
 var fs = require('fs');
-
-
 var consoleDataOutput = fs.createWriteStream('./output.txt');
+
+//現在時刻取得
+function time() {
+	var now = new Date();
+	var hour = now.getHours();
+	var min = now.getMinutes();
+	var sec = now.getSeconds();
+	//出力用
+	var s = hour + "," + min + "," + sec;
+	return s;
+}
 
 
 noble.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    noble.startScanning();
-  } else {
-    noble.stopScanning();
-  }
+  if (state === 'poweredOn') noble.startScanning();
+  else noble.stopScanning();
 });
+
+
 
 noble.on('discover', function(peripheral) {
 
-  console.log('Address', peripheral._noble.address);
+  //console.log('Address', peripheral._noble.address);
+  //console.dir(peripheral, {depth: null, colors: true});
+  //console.log()
 
-  console.dir(peripheral, {depth: null, colors: true});
-
-  console.log()
+  setInterval(function(){
+    console.log(time());
+    //console.log();
+    //console.log('Address', peripheral._noble.address);
+    //peripheral.updateRssi();
+  }, 1000);
 
   //output.txtにjson(Peripheral)書き込み
   consoleDataOutput.write(peripheral + '\n');
