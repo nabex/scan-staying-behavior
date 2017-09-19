820c9ad1236136e972f1d17f2ace76cbe1cd27d4
Koki Watanabe <gateway0616@gmail.com>
2017-09-19 13:16:23 +0900

    [add] consoleData [Peripheral]  output .txt

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var noble = require('noble');
+var fs = require('fs');
+
+
+var consoleDataOutput = fs.createWriteStream('./output.txt');
+
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
@@ -14,5 +19,8 @@ noble.on('discover', function(peripheral) {
 
   console.dir(peripheral, {depth: null, colors: true});
 
-  console.log();
+  console.log()
+
+  consoleDataOutput.write(peripheral + '\n');
+
 });
